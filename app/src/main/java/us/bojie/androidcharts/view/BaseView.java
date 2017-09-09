bdc693d9d742438b769d334f38227c5051227e88
Bojie Jiang <jiangbojie@gmail.com>
2017-09-09 12:56:30 -0700

    init Paint and width, height

--- app/src/main/java/us/bojie/androidcharts/view/BaseView.java
+++ app/src/main/java/us/bojie/androidcharts/view/BaseView.java
@@ -2,6 +2,8 @@ package us.bojie.androidcharts.view;
 
 import android.content.Context;
 import android.content.res.TypedArray;
+import android.graphics.Canvas;
+import android.graphics.Paint;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
@@ -13,6 +15,17 @@ import us.bojie.androidcharts.R;
  */
 
 public class BaseView extends View {
+
+    private Context mContext;
+    private Paint mPaint;
+
+    // View's width and height
+    private int width;
+    private int height;
+
+    private int originalX = 100;
+    private int originalY = 800;
+
     private String mGraphTitle;
     private String mXAxisName;
     private String mYAxisName;
@@ -30,7 +43,7 @@ public class BaseView extends View {
 
     public BaseView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
-
+        mContext = context;
         // Get custom style
         TypedArray typedArray = context.obtainStyledAttributes(attrs, R.styleable.GraphStyle);
 
@@ -45,5 +58,22 @@ public class BaseView extends View {
         if (typedArray != null) {
             typedArray.recycle();
         }
+
+        initPaint();
+    }
+
+    private void initPaint() {
+        if (mPaint == null) {
+            mPaint = new Paint();
+            mPaint.setDither(true);
+            mPaint.setAntiAlias(true);
+        }
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        width = getWidth() - originalX;
+        height = (originalY > getHeight() ? getHeight() : originalY) - 400;
     }
 }
